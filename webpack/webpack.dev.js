3ba24fee66153b745569eba3ba309bd1bf753a2b
cosocaf <cosocaf@gmail.com>
2022-05-04 21:40:12 +0900

    dev serverに対応

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,7 +1,21 @@
 const merge = require("webpack-merge");
-const common = require("./webpack.common")
+const common = require("./webpack.common");
+
+const path = require("path");
 
 module.exports = merge.merge(common, {
   mode: "development",
-  devtool: "source-map",
+  devtool: "inline-source-map",
+  devServer: {
+    static: path.join(__dirname, "..", "docs"),
+    compress: true,
+    port: 9000,
+    historyApiFallback: true,
+    open: true,
+    hot: true,
+    client: {
+      overlay: true,
+      progress: true,
+    },
+  }
 });
